f75f32398fc6a9e691651a98dbbf77706f3eec26
yeo-i <Zahraaa8000s@gmail.com>
2024-11-22 08:24:19 +0300

    Initial commit

---
+++ Project-Oop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Project_Oop
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public Book( string title ,string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+    }
+    class Library_catalogue
+    {
+        public string title;
+        public string author;
+
+        public void search() { }
+        public bool check() { return true; }
+        public Book add() { return new Book(title, author); }
+        public Library_catalogue() { }
+        
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
